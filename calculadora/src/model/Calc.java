c03f64131bed6e82ad2353c39bf5b001bf56b073
amxanda <91753947+amxanda@users.noreply.github.com>
2022-03-31 00:27:37 -0300

    Update Calc.java

--- calculadora/src/model/Calc.java
+++ calculadora/src/model/Calc.java
@@ -3,11 +3,13 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Calc {
+public class Calc 
+{
 	
 	private List<String> lastOperations = new ArrayList<String>();   
 	
-	public Number sum(Number valueOne, Number valueTwo) {
+	public Number sum(Number valueOne, Number valueTwo) 
+	{
 		Number result = valueOne.doubleValue() + valueTwo.doubleValue();
 		
 		saveOperation('+', valueOne, valueTwo, result);
@@ -15,7 +17,8 @@ public class Calc {
 		return result;
 	}
 
-	public Number sub(Number valueOne, Number valueTwo) {
+	public Number sub(Number valueOne, Number valueTwo) 
+	{
 		Number result = valueOne.doubleValue() - valueTwo.doubleValue();
 		
 		saveOperation('-', valueOne, valueTwo, result);
@@ -23,7 +26,8 @@ public class Calc {
 		return result;
 	}
 	
-	public Number div(Number valueOne, Number valueTwo) {
+	public Number div(Number valueOne, Number valueTwo) 
+	{
 		Number result = valueOne.doubleValue() / valueTwo.doubleValue();
 		
 		saveOperation('/', valueOne, valueTwo, result);
@@ -31,7 +35,8 @@ public class Calc {
 		return result;
 	}
 	
-	public Number mult(Number valueOne, Number valueTwo) {
+	public Number mult(Number valueOne, Number valueTwo) 
+	{
 		Number result = valueOne.doubleValue() * valueTwo.doubleValue();
 		
 		saveOperation('*', valueOne, valueTwo, result);
@@ -39,17 +44,20 @@ public class Calc {
 		return result;
 	}
 	
-	public List<String> getLastOperations(){
+	public List<String> getLastOperations()
+	{
 		List<String> copyLastOperations = new ArrayList<String>();
 		
-		for(String operation : lastOperations) {
+		for(String operation : lastOperations) 
+		{
 			copyLastOperations.add(operation);
 		}
 		
 		return copyLastOperations;
 	}
 	
-	private void saveOperation(char operation, Number valueOne, Number valueTwo, Number result) {
+	private void saveOperation(char operation, Number valueOne, Number valueTwo, Number result) 
+	{
 		String formattedResult = String.format("%.2f %s %.2f = %.2f", 
 				valueOne.doubleValue(), operation, valueTwo.doubleValue(), result);
 
